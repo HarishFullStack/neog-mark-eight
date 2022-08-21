24d489cae1b80dc8f0ab964aeb8337c787c85615
HarishFullStack <harishgupta.dev@gmail.com>
2022-08-21 20:27:55 +0530

    emoji apps

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+const emojiDictionary = {
+  "😊": "smiling",
+  "🤔": "thinking",
+  "😞": "sad",
+  "☎": "telephone",
+  "❤": "love",
+  "😑": "annoyance"
+};
+
+var emojisWeKnow = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+
+    var meaning = emojiDictionary[userInput];
+
+    if (meaning === undefined) {
+      meaning = "we don't have this in out database";
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+
+  return (
+    <div className="App">
+      <h1>inside outtt</h1>
+      <input onChange={emojiInputHandler} />
+
+      <h2>meaning {meaning}</h2>
+
+      <h3> emojis we know</h3>
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
